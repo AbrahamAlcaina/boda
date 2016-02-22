8aeadc670a9039d0f8311e7804631a56076f8175
Abraham Alcaina <abraham.alcaina@gmail.com>
2016-02-22 22:24:09 +0100

    (test) metas

--- src/containers/root.jsx
+++ src/containers/root.jsx
@@ -100,7 +100,7 @@ class Root extends Component {
           <meta property="og:title" content="Nicole i Abraham" />
           <meta property="og:description" content="Nos casamos el 28 de mayo" />
           <meta property="og:image" content="http://nicoleiabraham.com/img/meta.jpg" />
-          <meta property="og:url" content="http://nicoleiabraham.com" />
+          <meta property="og:url" content="http://nicoleiabraham.com/" />
           <meta property="og:type" content="website" />
         </head>
         <body style={styles.body}>
