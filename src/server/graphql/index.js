110c5691635ab7e4a8865de920543d955abaa158
Abraham Alcaina <abraham.alcaina@gmail.com>
2016-03-01 15:27:56 +0100

    fix

--- src/server/graphql/index.js
+++ src/server/graphql/index.js
@@ -2,6 +2,6 @@ import graphqlHTTP from 'express-graphql';
 import { schema } from './schema';
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://app:Marti2015@ds033744.mlab.com:33744/heroku_0b8kmf5g');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhosts');
 
 export const graphql = graphqlHTTP({ schema: schema, graphiql: true });
