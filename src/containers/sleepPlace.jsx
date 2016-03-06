03276696770a1c4505463dd6d9aa8ca94994fd2c
Abraham Alcaina <abraham.alcaina@gmail.com>
2016-03-06 18:41:14 +0100

    fix text

--- src/containers/sleepPlace.jsx
+++ src/containers/sleepPlace.jsx
@@ -11,7 +11,7 @@ class SleepPlace extends Component {
           <div className="row">
               <Helmet title="sleepPlace" />
               <div className="row">
-                <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-md-offset-2 col-lg-offset-2">
                   <h1>
                     <FormattedMessage
                       id="home.sleepPlace"
